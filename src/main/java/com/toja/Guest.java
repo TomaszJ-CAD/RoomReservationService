c825cc73f3ad6fb8b90bdaf917bd6a2a7552f5f8
TJ <cad.projekt.tj@gmail.com>
2024-08-21 00:00:37 +0200

    Added feature info about guest

--- src/main/java/com/toja/Guest.java
+++ src/main/java/com/toja/Guest.java
@@ -2,13 +2,21 @@ package com.toja;
 
 public class Guest {
 
-    String firstName;
-    String lastName;
-    int age;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private Gender gender;
 
-    public Guest(String fistName, String lastName, int age) {
+    public Guest(String fistName, String lastName, int age, Gender gender) {
         this.firstName = fistName;
         this.lastName = lastName;
         this.age = age;
+        this.gender = gender;
     }
+    // dzięki temu nie potrzebujemy getterów i setterów
+    public String getInfo() {
+
+        return String.format("Stworzono gościa: %s %s, %d lat, płeć %s.", this.firstName, this.lastName, this.age, this.gender);
+    }
+
 }

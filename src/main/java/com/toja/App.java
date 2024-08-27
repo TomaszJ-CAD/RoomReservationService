3579e31e6fe8eaef4f7d2ea7d2c04cf09df807a4
TJ <cad.projekt.tj@gmail.com>
2024-08-27 19:07:35 +0200

    Made clean in Add and TextUI classes

--- src/main/java/com/toja/App.java
+++ src/main/java/com/toja/App.java
@@ -1,7 +1,5 @@
 package com.toja;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class App {
 
@@ -13,91 +11,7 @@ public class App {
         int systemVersion = 1;
         boolean isDeveloperVersion = true;
 
-        showSystemInfo(hotelName, systemVersion, isDeveloperVersion);
-
-        Scanner input = new Scanner(System.in);
-
-        try {
-            performAction(input);
-        } catch (WrongOptionException | OnlyNumberException e) {
-            System.out.println("Wystąpił niespodziewany błąd");
-            System.out.println("Kod błędu: " + e.getCode());
-            System.out.println("Komunikat błędu: " + e.getMessage());
-            e.printStackTrace();
-        } catch (Exception e) {
-            System.out.println("Wystąpił niespodziewany błąd");
-            System.out.println("Nieznany kod błędu");
-            System.out.println("Komunikat błędu: " + e.getMessage());
-            e.printStackTrace();
-        } finally {
-            System.out.println("Wychodzę z aplikacji");
-        }
-
-
+        textUI.showSystemInfo(hotelName, systemVersion, isDeveloperVersion);
+        textUI.showMainMenu();
     }
-
-    private static void performAction(Scanner input) {
-        int option = getActionFromUser(input);
-
-
-        if (option == 1) {
-            textUI.readNewGuestData(input);
-
-        } else if (option == 2) {
-            textUI.readNewRoomData(input);
-
-        } else if (option == 3) {
-            System.out.println("Wybrano opcję 3");
-        } else {
-            throw new WrongOptionException("Wrong option in main menu.");
-        }
-    }
-
-    public static String developerVersion(boolean isDeveloperVersion) {
-        if (isDeveloperVersion == true) {
-            return "TAK";
-        } else {
-            return "NIE";
-        }
-    }
-
-    //TODO
-//    public static String genderForPrint(Gender gender) {
-//        if (gender == Gender.MALE) {
-//            return "męska";
-//        } else  {
-//            return "żeńska";
-//        }
-//    }
-
-    private static void showSystemInfo(String hotelName, int systemVersion, boolean isDeveloperVersion) {
-
-
-        String developerVersion = developerVersion(isDeveloperVersion);
-
-        System.out.println("Witam w systemie rezerwacji dla Hotelu " + hotelName);
-        System.out.println("Aktualna wersja systemu: " + systemVersion);
-        System.out.println("Wersja deweloperska: " + developerVersion);
-
-        System.out.println("\n===========================\n");
-    }
-
-    private static int getActionFromUser(Scanner in) {
-        System.out.println("1. Dodaj nowego gościa.");
-        System.out.println("2. Dodaj nowy pokój.");
-        System.out.println("3. Wyszukaj gościa.");
-        System.out.println("Wybierz opcję: ");
-
-        int actionNumber = 0;
-
-        try {
-            actionNumber = in.nextInt();
-
-        } catch (InputMismatchException e) {
-            throw new OnlyNumberException("Use only numbers in main menu");
-        }
-        return actionNumber;
-    }
-
-
 }
--- src/main/java/com/toja/TextUI.java
+++ src/main/java/com/toja/TextUI.java
@@ -31,6 +31,7 @@ public class TextUI {
 
         }
     }
+
     private static Gender chooseGenderType(Scanner in) {
         System.out.println("Podaj płeć gościa: ");
         System.out.println("\t1. mężczyzna");
@@ -98,5 +99,87 @@ public class TextUI {
             bedTypes[i] = bed;
         }
         return bedTypes;
+    }
+
+    public void showSystemInfo(String hotelName, int systemVersion, boolean isDeveloperVersion) {
+
+
+        String developerVersion = developerVersion(isDeveloperVersion);
+
+        System.out.println("Witam w systemie rezerwacji dla Hotelu " + hotelName);
+        System.out.println("Aktualna wersja systemu: " + systemVersion);
+        System.out.println("Wersja deweloperska: " + developerVersion);
+
+        System.out.println("\n===========================\n");
+    }
+
+    public static String developerVersion(boolean isDeveloperVersion) {
+        if (isDeveloperVersion == true) {
+            return "TAK";
+        } else {
+            return "NIE";
+        }
+    }
+    //TODO
+//    public static String genderForPrint(Gender gender) {
+//        if (gender == Gender.MALE) {
+//            return "męska";
+//        } else  {
+//            return "żeńska";
+//        }
+//    }
+
+    public void showMainMenu() {
+        Scanner input = new Scanner(System.in);
+
+        try {
+            performAction(input);
+        } catch (WrongOptionException | OnlyNumberException e) {
+            System.out.println("Wystąpił niespodziewany błąd");
+            System.out.println("Kod błędu: " + e.getCode());
+            System.out.println("Komunikat błędu: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Wystąpił niespodziewany błąd");
+            System.out.println("Nieznany kod błędu");
+            System.out.println("Komunikat błędu: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            System.out.println("Wychodzę z aplikacji");
+        }
+    }
+
+    private void performAction(Scanner input) {
+        int option = getActionFromUser(input);
+
+
+        if (option == 1) {
+            readNewGuestData(input);
+
+        } else if (option == 2) {
+            readNewRoomData(input);
+
+        } else if (option == 3) {
+            System.out.println("Wybrano opcję 3");
+        } else {
+            throw new WrongOptionException("Wrong option in main menu.");
+        }
+    }
+
+    private static int getActionFromUser(Scanner in) {
+        System.out.println("1. Dodaj nowego gościa.");
+        System.out.println("2. Dodaj nowy pokój.");
+        System.out.println("3. Wyszukaj gościa.");
+        System.out.println("Wybierz opcję: ");
+
+        int actionNumber = 0;
+
+        try {
+            actionNumber = in.nextInt();
+
+        } catch (InputMismatchException e) {
+            throw new OnlyNumberException("Use only numbers in main menu");
+        }
+        return actionNumber;
     }
 }
